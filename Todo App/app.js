68dfdaf8e98fc233cec23e20b7742f4a9e706d26
Yoghantara <gyoghantara@gmail.com>
2022-03-07 22:35:15 +0800

    push todo list di dalam ul

--- Todo App/app.js
+++ Todo App/app.js
@@ -1,5 +1,6 @@
 const inputBox = document.querySelector(".input-box input");
 const inputBtn = document.querySelector(".input-box button");
+const containerList = document.querySelector(".todo-box");
 
 inputBox.onkeyup = () => {
   let userData = inputBox.value;
@@ -9,6 +10,8 @@ inputBox.onkeyup = () => {
     inputBtn.classList.remove("active");
   }
 };
+
+showTodoList();
 
 inputBtn.addEventListener("click", () => {
   let userData = inputBox.value;
@@ -20,6 +23,24 @@ inputBtn.addEventListener("click", () => {
     storageArr = JSON.parse(getLocalStorage);
   }
 
-  storageArr.push(userData);
+  storageArr.push(userData); // push inputan user kedalam storage sa
   localStorage.setItem("New Todo", JSON.stringify(storageArr));
+  showTodoList();
 });
+
+// Tambah Todo List didalam ul
+function showTodoList() {
+  let getLocalStorage = localStorage.getItem("New Todo");
+  if (getLocalStorage == null) {
+    storageArr = []; // buat blank array
+  } else {
+    storageArr = JSON.parse(getLocalStorage); // transforming json string into js object
+  }
+
+  let newLi = "";
+  storageArr.forEach((element, index) => {
+    newLi += `<li>${element}<span onclick="deleteList(${index})"; ><i class="fa-solid fa-trash"></i></span></li>`;
+  });
+  containerList.innerHTML = newLi;
+  inputBox.value = "";
+}
