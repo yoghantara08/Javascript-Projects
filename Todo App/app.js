4892314d511114907cf04f3ebace9945b90a213c
Yoghantara <gyoghantara@gmail.com>
2022-03-07 21:58:37 +0800

    tambah onkeyup event saat input berisi text

--- Todo App/app.js
+++ Todo App/app.js
@@ -1,1 +1,11 @@
+const inputBox = document.querySelector(".input-box input");
+const inputBtn = document.querySelector(".input-box button");
 
+inputBox.onkeyup = () => {
+  let userData = inputBox.value; // user input value
+  if (userData.trim() != 0) {
+    inputBtn.classList.add("active");
+  } else {
+    inputBtn.classList.remove("active");
+  }
+};
