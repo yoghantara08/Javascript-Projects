3c0fe3534c7c47c18e894ee827971e9990cddc24
Yoghantara <gyoghantara@gmail.com>
2022-03-07 22:12:57 +0800

    menerima user input dan save ke localstorage

--- Todo App/app.js
+++ Todo App/app.js
@@ -2,10 +2,24 @@ const inputBox = document.querySelector(".input-box input");
 const inputBtn = document.querySelector(".input-box button");
 
 inputBox.onkeyup = () => {
-  let userData = inputBox.value; // user input value
+  let userData = inputBox.value;
   if (userData.trim() != 0) {
     inputBtn.classList.add("active");
   } else {
     inputBtn.classList.remove("active");
   }
 };
+
+inputBtn.addEventListener("click", () => {
+  let userData = inputBox.value;
+
+  let getLocalStorage = localStorage.getItem("New Todo");
+  if (getLocalStorage == null) {
+    storageArr = [];
+  } else {
+    storageArr = JSON.parse(getLocalStorage);
+  }
+
+  storageArr.push(userData);
+  localStorage.setItem("New Todo", JSON.stringify(storageArr));
+});
