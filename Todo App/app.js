6f1f8195f03923462c427fcdafeb5b610d834f38
Yoghantara <gyoghantara@gmail.com>
2022-03-07 22:44:15 +0800

    tambah fungsi remove todo list, clear all, dan tampilkan banyak pending tasks

--- Todo App/app.js
+++ Todo App/app.js
@@ -1,6 +1,7 @@
 const inputBox = document.querySelector(".input-box input");
 const inputBtn = document.querySelector(".input-box button");
 const containerList = document.querySelector(".todo-box");
+const clearAllBtn = document.querySelector(".footer button");
 
 inputBox.onkeyup = () => {
   let userData = inputBox.value;
@@ -37,6 +38,17 @@ function showTodoList() {
     storageArr = JSON.parse(getLocalStorage); // transforming json string into js object
   }
 
+  // isi value list pending
+  const listPending = document.querySelector(".pending");
+  listPending.textContent = storageArr.length;
+
+  // clear all button active
+  if (storageArr.length > 0) {
+    clearAllBtn.classList.add("active");
+  } else {
+    clearAllBtn.classList.remove("active");
+  }
+
   let newLi = "";
   storageArr.forEach((element, index) => {
     newLi += `<li>${element}<span onclick="deleteList(${index})"; ><i class="fa-solid fa-trash"></i></span></li>`;
@@ -44,3 +56,23 @@ function showTodoList() {
   containerList.innerHTML = newLi;
   inputBox.value = "";
 }
+
+// remove Todo List
+function deleteList(index) {
+  let getLocalStorage = localStorage.getItem("New Todo");
+  storageArr = JSON.parse(getLocalStorage);
+  storageArr.splice(index, 1);
+
+  // update local storage
+  localStorage.setItem("New Todo", JSON.stringify(storageArr));
+  showTodoList();
+}
+
+// clear all todo list
+clearAllBtn.addEventListener("click", () => {
+  storageArr = []; // kosongkan array
+
+  // update local storage
+  localStorage.setItem("New Todo", JSON.stringify(storageArr));
+  showTodoList();
+});
