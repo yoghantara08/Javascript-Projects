7fab2490ce99b5189406cc290f258ce53cf3f3e1
Yoghantara <gyoghantara@gmail.com>
2022-03-08 21:00:20 +0800

    add increment and reset events

--- Counter/app.js
+++ Counter/app.js
@@ -6,11 +6,39 @@ value.textContent = count;
 // Decrement
 const decrease = document.querySelector(".decrease");
 decrease.addEventListener("click", () => {
-  count -= 1;
+  count--;
 
   if (count < 0) {
     value.style.color = "red";
+  } else if (count == 0) {
+    black();
   }
   // update value
   return (value.textContent = count);
 });
+
+// Increment
+const increase = document.querySelector(".increase");
+increase.addEventListener("click", () => {
+  count++;
+
+  if (count > 0) {
+    value.style.color = "blue";
+  } else if (count == 0) {
+    black();
+  }
+  // update value
+  return (value.textContent = count);
+});
+
+// Reset
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+  count = 0;
+  black();
+  return (value.textContent = count);
+});
+
+function black() {
+  value.style.color = "black";
+}
