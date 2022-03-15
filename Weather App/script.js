7f3e07b592b51cb3340cc42baf29c4990e5b9b37
Yoghantara <gyoghantara@gmail.com>
2022-03-15 21:41:33 +0800

    weather app script

---
+++ Weather App/script.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", () => {
+  let longitude;
+  let latitude;
+  const countryRegion = document.querySelector(".country-region");
+  const degree = document.querySelector(".degree");
+  const description = document.querySelector(".description");
+  const times = document.querySelector(".localtime");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // Mendapatkan koordinat si pengguna
+      longitude = position.coords.longitude;
+      latitude = position.coords.latitude;
+
+      const api = `http://api.weatherapi.com/v1/current.json?key=111f232ff5624823bf4124932221503&q=${latitude},${longitude}`;
+
+      // Fetch API WeatherAPI
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const current = data.current;
+          const location = data.location;
+
+          // Current
+          const { temp_c, temp_f } = current;
+          const { text } = current.condition;
+          // Location
+          const { country, localtime, region } = location;
+
+          countryRegion.innerHTML = `${country}, ${region}`;
+          degree.innerHTML = `${temp_c}`;
+          description.innerHTML = `${text}`;
+          times.innerHTML = localtime;
+        });
+    });
+  }
+});
