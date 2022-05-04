91e137d89909aa3b860d6f43a8b2e88fe8f1914a
Yoghantara <gyoghantara@gmail.com>
2022-05-04 10:41:50 +0800

    navbar css

--- React/traveling-adventures/src/components/Navbar.js
+++ React/traveling-adventures/src/components/Navbar.js
@@ -2,6 +2,8 @@ import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 
+import "./Navbar.css";
+
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
