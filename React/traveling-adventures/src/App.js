2a7c3aec44503a634026e366208a92848817efba
Yoghantara <gyoghantara@gmail.com>
2022-05-04 12:38:55 +0800

    scroll to top

--- React/traveling-adventures/src/App.js
+++ React/traveling-adventures/src/App.js
@@ -7,18 +7,21 @@ import "./App.css";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Fragment>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </Fragment>
   );
