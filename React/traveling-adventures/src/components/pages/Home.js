e3cc0438f566a9d2b0fc9b830d9908c8311458f7
Yoghantara <gyoghantara@gmail.com>
2022-05-04 12:12:41 +0800

    cards component

--- React/traveling-adventures/src/components/pages/Home.js
+++ React/traveling-adventures/src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import "../../App.css";
+import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
