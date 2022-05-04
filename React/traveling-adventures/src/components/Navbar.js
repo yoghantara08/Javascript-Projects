5837ffa78f67880fd3180b329951e4bf119789bb
Yoghantara <gyoghantara@gmail.com>
2022-05-04 10:36:13 +0800

    button state

--- React/traveling-adventures/src/components/Navbar.js
+++ React/traveling-adventures/src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-  const handleClick = () => {
-    setClick(!click);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
   };
 
-  const closeMobileMenu = () => {
-    setClick(false);
-  };
+  window.addEventListener("resize", showButton);
 
   return (
     <Fragment>
@@ -56,6 +63,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </Fragment>
