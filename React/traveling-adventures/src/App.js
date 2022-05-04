ed8ddf3911663979fa79645b1a95dbb868a929a2
Yoghantara <gyoghantara@gmail.com>
2022-05-04 12:17:49 +0800

    add pages

--- React/traveling-adventures/src/App.js
+++ React/traveling-adventures/src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
 
 import "./App.css";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </Fragment>
